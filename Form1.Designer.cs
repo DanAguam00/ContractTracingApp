724db1a1bcda2d48cbef4ed358ba1a8947bf6c13
DanAguam00 <82425604+DanAguam00@users.noreply.github.com>
2021-06-04 11:47:56 +0800

    Labeled all objects and added a submit button.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,20 +31,21 @@ namespace ContractTracingApp
         {
             this.Lbl_FirstName = new System.Windows.Forms.Label();
             this.Lbl_LastName = new System.Windows.Forms.Label();
-            this.Lbl_Age = new System.Windows.Forms.Label();
+            this.Lbl_MiddleInitial = new System.Windows.Forms.Label();
             this.Lbl_Address = new System.Windows.Forms.Label();
             this.Lbl_ContactNumber = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.TxtBx_FirstName = new System.Windows.Forms.TextBox();
+            this.TxtBx_LastName = new System.Windows.Forms.TextBox();
+            this.TxtBx_Address = new System.Windows.Forms.TextBox();
+            this.TxtBx_MiddleInitial = new System.Windows.Forms.TextBox();
+            this.TxtBx_Age = new System.Windows.Forms.TextBox();
+            this.Lbl_Age = new System.Windows.Forms.Label();
+            this.TxtBx_Sex = new System.Windows.Forms.TextBox();
+            this.Lbl_Sex = new System.Windows.Forms.Label();
+            this.TxtBx_ContactNumber = new System.Windows.Forms.TextBox();
+            this.TxtBx_PWD = new System.Windows.Forms.TextBox();
+            this.Lbl_PWD = new System.Windows.Forms.Label();
+            this.Btn_Sbmt = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Lbl_FirstName
@@ -65,14 +66,14 @@ namespace ContractTracingApp
             this.Lbl_LastName.TabIndex = 1;
             this.Lbl_LastName.Text = "Last Name";
             // 
-            // Lbl_Age
+            // Lbl_MiddleInitial
             // 
-            this.Lbl_Age.AutoSize = true;
-            this.Lbl_Age.Location = new System.Drawing.Point(16, 120);
-            this.Lbl_Age.Name = "Lbl_Age";
-            this.Lbl_Age.Size = new System.Drawing.Size(28, 15);
-            this.Lbl_Age.TabIndex = 2;
-            this.Lbl_Age.Text = "Age";
+            this.Lbl_MiddleInitial.AutoSize = true;
+            this.Lbl_MiddleInitial.Location = new System.Drawing.Point(16, 120);
+            this.Lbl_MiddleInitial.Name = "Lbl_MiddleInitial";
+            this.Lbl_MiddleInitial.Size = new System.Drawing.Size(27, 15);
+            this.Lbl_MiddleInitial.TabIndex = 2;
+            this.Lbl_MiddleInitial.Text = "M.I.";
             // 
             // Lbl_Address
             // 
@@ -93,110 +94,121 @@ namespace ContractTracingApp
             this.Lbl_ContactNumber.Text = "Contact Number";
             this.Lbl_ContactNumber.Click += new System.EventHandler(this.Lbl_ContactNumber_Click);
             // 
-            // textBox1
+            // TxtBx_FirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(86, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 23);
-            this.textBox1.TabIndex = 5;
+            this.TxtBx_FirstName.Location = new System.Drawing.Point(86, 40);
+            this.TxtBx_FirstName.Name = "TxtBx_FirstName";
+            this.TxtBx_FirstName.Size = new System.Drawing.Size(288, 23);
+            this.TxtBx_FirstName.TabIndex = 5;
             // 
-            // textBox2
+            // TxtBx_LastName
             // 
-            this.textBox2.Location = new System.Drawing.Point(86, 79);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(288, 23);
-            this.textBox2.TabIndex = 6;
+            this.TxtBx_LastName.Location = new System.Drawing.Point(86, 79);
+            this.TxtBx_LastName.Name = "TxtBx_LastName";
+            this.TxtBx_LastName.Size = new System.Drawing.Size(288, 23);
+            this.TxtBx_LastName.TabIndex = 6;
             // 
-            // textBox3
+            // TxtBx_Address
             // 
-            this.textBox3.Location = new System.Drawing.Point(86, 155);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(288, 23);
-            this.textBox3.TabIndex = 7;
+            this.TxtBx_Address.Location = new System.Drawing.Point(86, 155);
+            this.TxtBx_Address.Name = "TxtBx_Address";
+            this.TxtBx_Address.Size = new System.Drawing.Size(288, 23);
+            this.TxtBx_Address.TabIndex = 7;
             // 
-            // textBox4
+            // TxtBx_MiddleInitial
             // 
-            this.textBox4.Location = new System.Drawing.Point(50, 117);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(32, 23);
-            this.textBox4.TabIndex = 8;
+            this.TxtBx_MiddleInitial.Location = new System.Drawing.Point(50, 117);
+            this.TxtBx_MiddleInitial.Name = "TxtBx_MiddleInitial";
+            this.TxtBx_MiddleInitial.Size = new System.Drawing.Size(32, 23);
+            this.TxtBx_MiddleInitial.TabIndex = 8;
             // 
-            // textBox5
+            // TxtBx_Age
             // 
-            this.textBox5.Location = new System.Drawing.Point(133, 117);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(32, 23);
-            this.textBox5.TabIndex = 10;
+            this.TxtBx_Age.Location = new System.Drawing.Point(133, 117);
+            this.TxtBx_Age.Name = "TxtBx_Age";
+            this.TxtBx_Age.Size = new System.Drawing.Size(32, 23);
+            this.TxtBx_Age.TabIndex = 10;
             // 
-            // label1
+            // Lbl_Age
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(99, 120);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 15);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Sex";
+            this.Lbl_Age.AutoSize = true;
+            this.Lbl_Age.Location = new System.Drawing.Point(99, 120);
+            this.Lbl_Age.Name = "Lbl_Age";
+            this.Lbl_Age.Size = new System.Drawing.Size(28, 15);
+            this.Lbl_Age.TabIndex = 9;
+            this.Lbl_Age.Text = "Age";
             // 
-            // textBox6
+            // TxtBx_Sex
             // 
-            this.textBox6.Location = new System.Drawing.Point(224, 117);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(32, 23);
-            this.textBox6.TabIndex = 12;
-            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.TxtBx_Sex.Location = new System.Drawing.Point(214, 117);
+            this.TxtBx_Sex.Name = "TxtBx_Sex";
+            this.TxtBx_Sex.Size = new System.Drawing.Size(32, 23);
+            this.TxtBx_Sex.TabIndex = 12;
+            this.TxtBx_Sex.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
-            // label2
+            // Lbl_Sex
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(190, 120);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 15);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Sex";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.Lbl_Sex.AutoSize = true;
+            this.Lbl_Sex.Location = new System.Drawing.Point(182, 120);
+            this.Lbl_Sex.Name = "Lbl_Sex";
+            this.Lbl_Sex.Size = new System.Drawing.Size(25, 15);
+            this.Lbl_Sex.TabIndex = 11;
+            this.Lbl_Sex.Text = "Sex";
+            this.Lbl_Sex.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox7
+            // TxtBx_ContactNumber
             // 
-            this.textBox7.Location = new System.Drawing.Point(118, 194);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(256, 23);
-            this.textBox7.TabIndex = 13;
+            this.TxtBx_ContactNumber.Location = new System.Drawing.Point(118, 194);
+            this.TxtBx_ContactNumber.Name = "TxtBx_ContactNumber";
+            this.TxtBx_ContactNumber.Size = new System.Drawing.Size(256, 23);
+            this.TxtBx_ContactNumber.TabIndex = 13;
             // 
-            // textBox8
+            // TxtBx_PWD
             // 
-            this.textBox8.Location = new System.Drawing.Point(316, 117);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(32, 23);
-            this.textBox8.TabIndex = 15;
+            this.TxtBx_PWD.Location = new System.Drawing.Point(302, 117);
+            this.TxtBx_PWD.Name = "TxtBx_PWD";
+            this.TxtBx_PWD.Size = new System.Drawing.Size(72, 23);
+            this.TxtBx_PWD.TabIndex = 15;
             // 
-            // label3
+            // Lbl_PWD
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(282, 120);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 15);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "PWD";
+            this.Lbl_PWD.AutoSize = true;
+            this.Lbl_PWD.Location = new System.Drawing.Point(262, 120);
+            this.Lbl_PWD.Name = "Lbl_PWD";
+            this.Lbl_PWD.Size = new System.Drawing.Size(33, 15);
+            this.Lbl_PWD.TabIndex = 14;
+            this.Lbl_PWD.Text = "PWD";
+            // 
+            // Btn_Sbmt
+            // 
+            this.Btn_Sbmt.Location = new System.Drawing.Point(162, 255);
+            this.Btn_Sbmt.Name = "Btn_Sbmt";
+            this.Btn_Sbmt.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Sbmt.TabIndex = 16;
+            this.Btn_Sbmt.Text = "Submit";
+            this.Btn_Sbmt.UseVisualStyleBackColor = true;
+            this.Btn_Sbmt.Click += new System.EventHandler(this.Btn_Sbmt_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 450);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btn_Sbmt);
+            this.Controls.Add(this.TxtBx_PWD);
+            this.Controls.Add(this.Lbl_PWD);
+            this.Controls.Add(this.TxtBx_ContactNumber);
+            this.Controls.Add(this.TxtBx_Sex);
+            this.Controls.Add(this.Lbl_Sex);
+            this.Controls.Add(this.TxtBx_Age);
+            this.Controls.Add(this.Lbl_Age);
+            this.Controls.Add(this.TxtBx_MiddleInitial);
+            this.Controls.Add(this.TxtBx_Address);
+            this.Controls.Add(this.TxtBx_LastName);
+            this.Controls.Add(this.TxtBx_FirstName);
             this.Controls.Add(this.Lbl_ContactNumber);
             this.Controls.Add(this.Lbl_Address);
-            this.Controls.Add(this.Lbl_Age);
+            this.Controls.Add(this.Lbl_MiddleInitial);
             this.Controls.Add(this.Lbl_LastName);
             this.Controls.Add(this.Lbl_FirstName);
             this.Name = "Form1";
@@ -210,20 +222,21 @@ namespace ContractTracingApp
 
         private System.Windows.Forms.Label Lbl_FirstName;
         private System.Windows.Forms.Label Lbl_LastName;
-        private System.Windows.Forms.Label Lbl_Age;
+        private System.Windows.Forms.Label Lbl_MiddleInitial;
         private System.Windows.Forms.Label Lbl_Address;
         private System.Windows.Forms.Label Lbl_ContactNumber;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtBx_FirstName;
+        private System.Windows.Forms.TextBox TxtBx_LastName;
+        private System.Windows.Forms.TextBox TxtBx_Address;
+        private System.Windows.Forms.TextBox TxtBx_MiddleInitial;
+        private System.Windows.Forms.TextBox TxtBx_Age;
+        private System.Windows.Forms.Label Lbl_Age;
+        private System.Windows.Forms.TextBox TxtBx_Sex;
+        private System.Windows.Forms.Label Lbl_Sex;
+        private System.Windows.Forms.TextBox TxtBx_ContactNumber;
+        private System.Windows.Forms.TextBox TxtBx_PWD;
+        private System.Windows.Forms.Label Lbl_PWD;
+        private System.Windows.Forms.Button Btn_Sbmt;
     }
 }
 
